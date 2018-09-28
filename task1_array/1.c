e536607c37bb1731a3cdb027b704ac98dd010115
Mikhail <tikhonov_mikhail@hotmail.com>
2018-09-28 15:17:42 +0400

    First program done

--- task1_array/1.c
+++ task1_array/1.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 
+#define N 6
+
+void print_array(int a[][N]) {
+	int i = NULL, j = NULL;
+	for (i = 0; i < N; i++) {
+	for (j = 0; j < N; j++) {
+		printf("%d    ",a[i][j]);
+		}
+	printf("\n");
+	}
+}
+
 int main() {
-	cout<<"Hello world!";
+	int a[N][N];
+	int count = 1;
+	int i = NULL, j = NULL;
+	for (i = 0; i < N; i++) 
+	for (j = 0; j < N; j++) {
+		a[i][j] = count;
+		count++;
+	}
+	printf("Before\n");
+	print_array(a);
+	for (i = 0; i < N; i++)
+	for (j = 0; j < N; j++) {
+		if (j%2==0) {
+			a[i][j+1] = a[i][j] + a[i][j+1];
+			a[i][j] = a[i][j+1] - a[i][j];
+			a[i][j+1] = a[i][j+1] - a[i][j]; 
+		}
+	}
+	printf("After\n");
+	print_array(a);
 	return 0;
 }
